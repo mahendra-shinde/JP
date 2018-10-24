26d63895340316cf74d129bf6da95332b8ce15ab
Administrator <swati.palkar@jpmorgan.com>
2018-10-24 16:26:21 +0530

    Assesment2

---
+++ swati/Assesment2/src/com/swati/User.java
@@ -0,0 +1,47 @@
+package com.swati;
+
+public class User {
+    private String username;
+    private String password;
+    private String role;
+    private String active;
+
+    public User(String username, String password, String role, String active) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.active = active;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getActive() {
+        return active;
+    }
+
+    public void setActive(String active) {
+        this.active = active;
+    }
+}
